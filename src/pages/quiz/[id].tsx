d4b81638bc2f9d17135036644f40dede79a89a37
Roman Tarnai <roman.tarnai.04@gmail.com>
2023-03-21 19:45:29 +0100

    modal

--- src/pages/quiz/[id].tsx
+++ src/pages/quiz/[id].tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Question from "../../components/Question";
 import { IQuiz } from "../../../types/types";
-import { useAppDispatch } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { setMultiple } from "../../app/questionSlice";
 
 function Quiz() {
@@ -13,6 +13,7 @@ function Quiz() {
     const [multiple, setMultipleq] = useState<boolean>();
     const [questionId, setQuestionId] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(true);
+    const [modal, setModal] = useState<boolean>(false);
     const { id } = router.query;
 
     async function fetchQuiz() {
@@ -54,15 +55,84 @@ function Quiz() {
         }
     }
 
-    function handleBtnSubmit() {}
+    function handleSureSubmit() {
+        setModal(false);
+        setQuestionId(0);
+    }
 
-    // TODO: create a are you sure modal for submit
+    // TODO: store answers to questions (redux?)
 
     console.log(id);
 
     return (
         <>
-            <div className="fixed translate-x-1/2 translate-y-1/2 inset-1/2"></div>
+            {modal && (
+                <div className="customModal fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                    <div className="relative w-full h-full max-w-2xl md:h-auto">
+                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <button
+                                onClick={() => setModal(false)}
+                                type="button"
+                                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+                                data-modal-hide="popup-modal"
+                            >
+                                <svg
+                                    aria-hidden="true"
+                                    className="w-5 h-5"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"
+                                    ></path>
+                                </svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                            <div className="p-6 text-center">
+                                <svg
+                                    aria-hidden="true"
+                                    className="mx-auto mb-4 text-gray-200 w-14 h-14 "
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    ></path>
+                                </svg>
+                                <h3 className="mb-5 text-lg font-normal text-gray-400 ">
+                                    Are you sure you want to submit?<br></br> You won't be able
+                                    to change your selection after.
+                                </h3>
+                                <button
+                                    onClick={() => handleSureSubmit()}
+                                    data-modal-hide="popup-modal"
+                                    type="button"
+                                    className="text-white bg-yellow-600 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-yellow-700 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                                >
+                                    Yes, I'm sure
+                                </button>
+                                <button
+                                    onClick={() => setModal(false)}
+                                    data-modal-hide="popup-modal"
+                                    type="button"
+                                    className="text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-500 rounded-lg border border-gray-500 text-sm font-medium px-5 py-2.5 hover:text-gray-100 focus:z-10"
+                                >
+                                    No, cancel
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
             <div className="w-5/6 m-auto mt-6 text-white">
                 {loading ? (
                     <div className="lds-ring">
@@ -104,21 +174,21 @@ function Quiz() {
                         <div className="flex flex-row justify-between mt-12">
                             <button
                                 onClick={() => handleBtnPrev()}
-                                className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-sm hover:bg-yellow-600"
+                                className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-md hover:bg-yellow-600"
                             >
                                 Prev
                             </button>
                             {questionId + 1 == quiz?.questions.length ? (
                                 <button
-                                    onClick={() => handleBtnSubmit()}
-                                    className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-sm hover:bg-yellow-600"
+                                    onClick={() => setModal(true)}
+                                    className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-md hover:bg-yellow-600"
                                 >
                                     Submit
                                 </button>
                             ) : (
                                 <button
                                     onClick={() => handleBtnNext()}
-                                    className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-sm hover:bg-yellow-600"
+                                    className="w-40 px-8 py-2 text-xl text-black bg-yellow-500 rounded-md hover:bg-yellow-600"
                                 >
                                     Next
                                 </button>
